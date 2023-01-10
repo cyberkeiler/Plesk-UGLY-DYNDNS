72f04fba995eff8555d72ae7af2ae19586b3d39a
cyberkeiler <toby@terramultimedia.de>
2023-01-10 20:32:32 +0100

    Update changeDNS.sh
    Now using new IP file format and checking against active A/AAAA records

--- changeDNS.sh
+++ changeDNS.sh
@@ -1,52 +1,94 @@
+#!/bin/bash
+
 # Your configuration
-IPPATH="/var/yourIPfolder" # The Path to the files with the IP are stored!
 DOMAIN="yourdomain.com"
+IP_FOLDER="/var/www/vhosts/${DOMAIN}" # The Path to the files with the IP are stored!
+TMP_FILE="./exists.tmp"
 
 # Defining Colors
 NOC='\033[0m'
-RED='\033[0;31m'
-COLOR2='\033[0;35m'
-COLOR1='\033[1;33m'
+cRED='\033[0;31m'
+cOK='\033[0;32m'
+cPINK='\033[0;35m'
+cYEL='\033[1;33m'
 COLOR9='\033[1;37m'
 
-echo -e  "${COLOR2}##################################\n##  THE UGLY DYNDNS-SCRIPT 0.1  ##\n##################################${NOC}"
+# To loop over arrays
+ENTRIES=("A" "AAAA")
 
+# Helper functions for printig debug and error messages
+debug() {
+        echo -e "${NOC}\e[7m[DEBUG] $@ ${NOC}"
+}
+error() {
+        echo -e "${cRED}\e[7m[🚨 ERROR] $@ ${NOC}"
+}
 
-for SUBDOMAIN in "$@"
-do
-        # The Path to the IP files
-        FILE_NEW="${IPPATH}/${SUBDOMAIN}_NEWIP.txt"
-        FILE_OLD="${IPPATH}/${SUBDOMAIN}_OLDIP.txt"
+# Print Header
+echo -e "${cPINK}\e[7m##################################\n##  THE UGLY DYNDNS-SCRIPT 0.2  ##\n##################################${NOC}\n"
 
-        if [ !  -f "$FILE_NEW" ]
-                then printf "${RED}ERROR!!!${COLOR1} IP File for ${COLOR2}${SUBDOMAIN}${COLOR1}.${DOMAIN} ${COLOR1}does not exist!\n - Searched for:${COLOR9} ${FILE_NEW}\n"
-        else
-                # Excerting the IPs out of the files
-                IP_NEW=`cat ${FILE_NEW}`
+# Info Message when no arguments passed
+if [ $# -eq 0 ]; then
+        error "EMPTY ARGUMENTS"
+        echo -e "Please add desired subdomains as arguments\n e.g. ${cYEL}./changeDNS.sh subdomain1 subdomain2${cRED} to update ${cPINK}subdomain1.${DOMAIN}${cRED} and ${cPINK}subdomain2.${DOMAIN}.${NOC}"
+fi
 
-                if [  -f "$FILE_OLD" ]
-                        then IP_OLD=`cat ${FILE_OLD}`
-                fi
+for SUBDOMAIN in "$@"; do
+        for ENTRY in "${ENTRIES[@]}"; do
+                # Create the path of IP_FILE
+                IP_FILE="${IP_FOLDER}/${SUBDOMAIN}_${ENTRY}.txt"
 
-                #Check if the IPs did change:
-                if [ "$IP_NEW" == "$IP_OLD" ]
-                        then echo -e "${COLOR1}The IP for ${COLOR2}${SUBDOMAIN}${COLOR1}.${DOMAIN}. did not change! It's still ${COLOR2}${IP_NEW} ${COLOR9}"
+                if [ ! -f "$IP_FILE" ]; then
+                        # IP_FILE does not exist
+                        error "No (IP)-File found:${cPINK} ${SUBDOMAIN}${cYEL}.${DOMAIN} ${cYEL}${COLOR9}should be in ${IP_FILE}\n"
                 else
-                        if [  -f "$FILE_OLD" ]
-                                then # This will delete the A Entry of the Subdomain to the old IP Adress
-                                echo -e "${COLOR1}Delete linking A-Entry of ${COLOR2} ${SUBDOMAIN}.${DOMAIN} ${COLOR9}"
-                                /usr/local/psa/bin/dns --del ${DOMAIN} -a $SUBDOMAIN -ip $IP_OLD
-                        else
-                                echo -e "${COLOR1}No OLD IP File - so I do not know any Entry to delete for ${COLOR2} ${SUBDOMAIN}.${DOMAIN} ${COLOR1}yet${COLOR9}"
+                        # read new IP
+                        IP_NEW=$(cat ${IP_FILE})
+                        IP_NEW_AGE=$((($(date +%s) - $(date +%s -r "${IP_FILE}")) / 60))
+                        echo "false" >${TMP_FILE}
+                        if (("$IP_NEW_AGE" > 20)); then
+                                echo -e "${cYEL}\e[7m[WARNING] IP maybe old: ${IP_FILE} updated >${IP_NEW_AGE} minutes ago\n"
                         fi
 
-                        echo -e "${COLOR1}Recreate Linking ${COLOR2} ${SUBDOMAIN}.${DOMAIN} ${COLOR9}"
+                        # check current entries in PLESKs DNS, delete old ones and only keep those with current IP
+                        /usr/local/psa/bin/dns --info ${DOMAIN} | grep "${SUBDOMAIN}.${DOMAIN}. ${ENTRY} " |
+                                while IFS= read -r line; do
+
+                                        # read IP of A/AAAA entry
+                                        IP=$(echo "${line}" | cut -d " " -f 4)
+
+                                        echo -e -n "${cPINK}${SUBDOMAIN}${cYEL}.${DOMAIN}${NOC} | ${cPINK}${ENTRY}${NOC} |  "
+                                        if [ "$IP_NEW" == "$IP" ]; then
+                                                echo -e "${cOK}${IP} ✅"
+                                                echo "true" >${TMP_FILE}
+                                                # there is an entry with current IP, so only old entries have to be deleted, but nothing to be added
+
+                                        else
+                                                echo -e -n "${cRED}${IP} - outdated 💀 => "
+
+                                                if $(/usr/local/psa/bin/dns --del ${DOMAIN} -${ENTRY,,} $SUBDOMAIN -ip $IP); then
+                                                        echo -e " ${cOK}DELETED 🗑️${NOC} "
+                                                else
+                                                        error "DELETING FAILED"
+                                                fi
+
+                                                debug ${RESULT}
+                                        fi
+                                done
+
                         # Replace Old IP by new IP
-                        if /usr/local/psa/bin/dns --add ${DOMAIN} -a ${SUBDOMAIN} -ip ${IP_NEW}
-                                then cp $FILE_NEW $FILE_OLD
-                        else
-                                echo -e " - ${RED} FAILED!"
+
+                        if [[ $(cat ${TMP_FILE}) = "false" ]]; then
+                                echo -e -n " 🌟 New ${cPINK}${ENTRY} ${NOC} Entry required for ${cPINK}${SUBDOMAIN}${cYEL}.${DOMAIN}${NOC}: \n"
+
+                                if /usr/local/psa/bin/dns --add ${DOMAIN} -${ENTRY,,} ${SUBDOMAIN} -ip ${IP_NEW}; then
+                                        echo -e "=> ${cOK}SUCESSFULLY CREATED ✅"
+                                else
+                                        error "While adding ${ENTRY} entry 🚨 "
+                                fi
                         fi
+                        echo -e -n $NOC
                 fi
-        fi
+        done
 done
+rm ${TMP_FILE}
